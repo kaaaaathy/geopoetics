54563a23cb129ff78d08e6e435e00f773412032e
kathy wu <kaaathywu@gmail.com>
2024-08-09 08:34:19 -0500

    add

---
+++ projects.js
@@ -0,0 +1,119 @@
+export default
+[ {
+  shown:false,
+  date:"",
+  project:"+",
+  name:"Geo-scores",
+  content:`
+  (In class)
+  <br>
+PART 1: Think and write about your geologic object – what came before and what may come after it. What sound does it make, what vibrations does it remember? Listen to your object and write 1 page.
+<br>
+<br>
+(After class)
+<br>
+PART 2:  1-2 pages. Write several scores (or sets of instructions) to be completed with your geologic object. Perform the score.  Document the performance and show it alongside the score, whether it is through writing, photographs, sound recordings, etc.
+<br><br>
+Inspiration:
+<li>Crystal Text (excerpt) by Clark Coolidge
+<li>(Soma)tic Exercise #42 by CA Conrad
+<li>CA Conrad & Hoa Nguyen on Crystals, Crows, Cannibalizing Poems (excerpt)
+<li>Yoko Ono, excerpts from Grapefruit (Snow piece, Walking piece)
+<br><br>
+Listen:
+<li>Pauline Oliveros, Deep Listening
+<li>Alvin Lucier, I Am Sitting in a Room
+<li>The Music of Ice - playlist by nytimes`
+  },
+  {
+    shown:false,
+    date:"",
+    project:"+",
+    name:"Fossil Frottage",
+    content:`
+    Frottage exercise: Do graphite/charcoal rubbings of a series object which will survive us. Do between 5-10, and think about why this particular series of objects. Are they united by place, by memory, are they human-made, organic? Do they exist on vastly different or similar timescales? If you would like to explore color, or other methods of leaving a mark or trace, you are welcome to stray from the graphite/charcoal.
+<br><br>
+Write a short poem or piece of prose alongside this, one for each. Write also about a collective or individual memory. Can you integrate text into the fossilized object as well?
+    `
+    },
+
+    {
+      shown:false,
+      date:"",
+      project:"+",
+      name:"10,000 Years",
+      content:`Create a work designed to be experienced over 10,000 years.
+      <br><br>
+      Make something durational which extends beyond a human-sized timescale. Think about site / material / speculative fictions. You can express this through photography, drawing, photo collage, photoshop, writing, any media of your choice.
+
+`
+    },
+{
+		shown:false,
+    date:"",
+		project:"+",
+    name:"Mineral Research",
+		content:`Forage together research on a particular mined stone or mineral, to share with the class. Gather 2-3 pages of quotes, citations, and 5-10 images. Be prepared to share 1-2 pieces of writing or artwork which engages with your mineral. Where does it come from? Who buys it? What have humans used this for, and for how long? What are its geopolitical dimensions? Be prepared to read this to the class; present your metal/stone to the class in a 5 minute reading that engages at least one other sensory dimension (sound, image… taste if you like… texture)
+    `
+	},
+	{
+		shown:false,
+    date:"",
+		project:"+",
+    name:"Transformation",
+		content:`PATH ONE
+    <br><br>Related to your mineral: transform or translate an existing text – like a legal document, a news article, a data set. Use ink, or scissors, or glue, or heat, or photo processing, or…. Try a process you haven’t done before.
+    For both assignments: Write down a list of as many relevant verbs as you can think of in 10 minutes. Post this list to canvas.
+    <br><br>Potential resources
+    <li>https://mrdata.usgs.gov/mrds/find-mrds-graded.php
+    <li>Google News (search for your mineral, or things very adjacent to your mineral)
+    <li>The library / used book store
+     
+    <br><br>
+    PATH TWO (for those who need respite from making)
+    <br>
+    Think about transformation at a broader scale. Think of a few possible things thing you want to do this week which would help people get what they deserve / their needs met. Think of one way that you could help do that for yourself. Do those things. Won’t ask you to turn this in. But do write about what a sustainable practice of transformation looks like, about 1-2 paragraphs.
+    <br><br>
+    For both assignments: Write down a list of as many relevant verbs as you can think of in 10 minutes. (See: Richard Serra Verb List)
+     
+    `
+
+	},
+  {
+		shown:false,
+    date:"",
+		project:"+",
+    name:"Concrete Landscapes",
+		content:`Write three poems on either erosion or eruption. Taking a moment to reflect on poems such as Kamau Braithwaite's BREAD, or Laura Moriarty's PERSONAL VOLCANO, can you pay extra attention to the shape of your poem? The enjambments, how they create slowness or speed. Maybe you are interested in the typographic rag of your poem as a kind of shoreline? Or a concrete poetics where each poem is a contour of land? Think about these questions as you write your poem.`
+	},
+	{
+		shown:false,
+    date:"",
+		project:"+",
+    name:"A Long Line",
+		content:`Take a walk within the week. Modify something small in your landscape and document it. Using one continuous line, can you create a map of that walk?
+    <br><br>Inspired by Eleni Sikelianos.`
+	},
+  {
+		shown:false,
+    date:"",
+		project:"+",
+    name:"Map Poetry",
+		content:`Inspired by Damian Zucconi, Meiko Shiomi, Queering the Map, and other projects, can you take an existing map and tracing paper and create a new concrete poem derived from it? 
+    <br><br>
+    First, you might trace + write only the text as it is. Think: What is missing from the original map? What could be named something else? What should not be represented on this map?
+ <br><br>Next, take 1-2 of the names, and using a computer or phone, research the background of that place. Could use Native-Land.ca, or something else. What is it called now, what did it used to be called? When did that change? Have there been any efforts to rename? Take some notes
+Write a short poem about your findings.<br><br>
+Finally, using a second sheet of tracing paper on the original map, rewrite the original text and with your own text. Erase, delete, modify, or respell to create your counter-cartographic poem.
+<br><br>
+Much love and gratitude to Hua Xi for teaching me about spatial poetry.`
+      },
+	{
+		shown:false,
+    date:"",
+		project:"+",
+    name:"Final Project Proposal",
+		content:`Turn in a 2-page plan with 5+ references, 5+ images, and 2+ reading quotes for our final project. What geologic artwork or intervention or meditation will you create for the final? Will you modify an existing prompt or redo a previous prompt? Will you use your research to inform this work? What will you show at the final class?`
+	},
+
+]
